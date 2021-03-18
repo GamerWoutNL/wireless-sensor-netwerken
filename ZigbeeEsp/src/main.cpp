f84e4c74a2dfc20434807e3d5e8707f773f8a8da
Wout <woutstevens@live.nl>
2021-03-18 16:15:51 +0100

    Added mqtt client

--- ZigbeeEsp/src/main.cpp
+++ ZigbeeEsp/src/main.cpp
@@ -1,14 +1,54 @@
 #include <Arduino.h>
 #include <HardwareSerial.h>
 #include <XBee.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
 
 #define LED_GREEN 18
 #define LED_RED 19
 
+WiFiClient client;
+PubSubClient mqtt;
 HardwareSerial XbeeSerial(1);
-
 XBee xbee = XBee();
 ZBRxResponse response = ZBRxResponse();
+
+const char* wifi_ssid = "FRITZ!Box Fon WLAN 7360";
+const char* wifi_password = "StevensLegstraat";
+const char* mqtt_server = "test.mosquitto.org";
+const int mqtt_port = 1883;
+const char* mqtt_topic = "smartmeter-wout";
+
+void connectWifi() {
+  delay(10);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(wifi_ssid, wifi_password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+
+  WiFi.setAutoReconnect(true);
+  WiFi.persistent(true);
+}
+
+void connectMQTT() {
+  mqtt.setClient(client);
+  mqtt.setBufferSize(2048);
+  mqtt.setServer(mqtt_server, mqtt_port);
+
+  while (!mqtt.connected()) {
+    mqtt.connect("");
+    mqtt.subscribe(mqtt_topic);
+    
+    delay(500);
+  }
+}
+
+void publishMQTT(String payload) {
+  mqtt.publish(mqtt_topic, payload.c_str());
+}
 
 void flashLed(int pin, int times, int wait) {
   for (int i = 0; i < times; i++) {
@@ -25,17 +65,19 @@ void flashLed(int pin, int times, int wait) {
 void setup() {
   Serial.begin(9600);
 
+  connectWifi();
+  connectMQTT();
+
   XbeeSerial.begin(9600, SERIAL_8N1, 16, 17);
   xbee.begin(XbeeSerial);
 
   pinMode(LED_GREEN, OUTPUT);
   pinMode(LED_RED, OUTPUT);
-
-  flashLed(LED_GREEN, 3, 50);
-  flashLed(LED_RED, 3, 50);
 }
 
 void loop() {
+  mqtt.loop();
+
   xbee.readPacket();
 
   if (xbee.getResponse().isAvailable()) {
@@ -52,8 +94,10 @@ void loop() {
       Serial.print("Humidity: ");
       Serial.print(humidity);
       Serial.println(" %");
-      
+
       Serial.println();
+
+      publishMQTT(String(temperature));
 
       flashLed(LED_GREEN, 1, 1000);
     } else {
